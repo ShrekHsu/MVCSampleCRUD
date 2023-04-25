4f1deeecb1d8806b45b23b3163e93aefee61ba02
shrekHsu <sky19key@gmail.com>
2023-04-25 20:49:37 +0800

    DB& CRUD-R

--- MVCSampleCRUD/Controllers/HomeController.cs
+++ MVCSampleCRUD/Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCSampleCRUD.Models;
 
 namespace MVCSampleCRUD.Controllers
 {
     public class HomeController : Controller
     {
+        //呼叫 DB
+        Database1Entities DB01 = new Database1Entities();
+
         public ActionResult Index()
         {
-            return View();
+            var DB = DB01.Datebase1.OrderByDescending(m => m.fDate).ToList();
+
+            return View(DB);
         }
 
         public ActionResult About()
