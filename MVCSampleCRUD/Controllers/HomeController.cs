208228e42f295e85ebc9cc2c330f21239e93ab68
shrekHsu <sky19key@gmail.com>
2023-04-25 21:26:10 +0800

    CRUD-C

--- MVCSampleCRUD/Controllers/HomeController.cs
+++ MVCSampleCRUD/Controllers/HomeController.cs
@@ -18,7 +18,23 @@ namespace MVCSampleCRUD.Controllers
 
             return View(DB);
         }
+        public ActionResult Create()
+        {
 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string fTitle,string fImage, DateTime fDate)
+        {
+            Datebase1 DB1 = new Datebase1();
+            DB1.fTitle = fTitle;
+            DB1.fImage = fImage;
+            DB1.fDate = fDate;
+            DB01.Datebase1.Add(DB1);
+            DB01.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
